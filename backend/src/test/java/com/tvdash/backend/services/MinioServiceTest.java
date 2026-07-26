76cd4de5ec1c8c969f6f8456e0c47270fd172755
lirad0 <292372902+lirad0@users.noreply.github.com>
2026-07-26 12:09:30 +0200

    fix

--- backend/src/test/java/com/tvdash/backend/services/MinioServiceTest.java
+++ backend/src/test/java/com/tvdash/backend/services/MinioServiceTest.java
@@ -1,18 +1,16 @@
 package com.tvdash.backend.services;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import static org.mockito.ArgumentMatchers.any;
 import org.mockito.Mock;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 
 import com.tvdash.backend.config.MinioProperties;
